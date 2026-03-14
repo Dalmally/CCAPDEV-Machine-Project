e095b16e652683896d8e77a5c6990d7f9c664bb1
Dalmally <juwan.hernane@gmail.com>
2026-03-14 19:20:51 +0800

    Added some controllers

---
+++ controller/commentController.js
@@ -0,0 +1,31 @@
+const Comment = require('../model/Comment')
+const Post = require('../models/Post')
+
+//This will mainly add and get posts from the database
+
+exports.addComment = async (req, res) => {
+    
+    /**
+     * Algorithm might go something like
+     * 
+     * get the postid
+     * 
+     * get the content and the comment author
+     * 
+     * save into db 
+     * 
+     * redirect it to the same page so user can see their own comments
+     */
+}
+
+exports.getComments = async (req, res) => {
+    /**
+     * Algorithm should be
+     * 
+     * get comments from db based on postid
+     * 
+     * format according to the comment section in viewpost.hbs
+     * 
+     * push as json
+     */
+}
